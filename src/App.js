352ff20d4bfbf91a06cfc538cbdcb7abda6d60a7
Callum Lees <hi@c-l.dev>
2022-04-23 14:28:10 +0100

    Imported Card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import Card from './components/Card';
 
 const App = () => {
 
   return (
-  
-    <div className="">
-   
-    </div>
-  
+ 
+    <>
+     <Card /> 
+    </> 
   );
 
 }
